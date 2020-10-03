cd381274ae369e864a1c1c66f72e5c7643c97ed7
Natia <n.nakashidze09@gmail.com>
2020-10-03 19:26:08 +0400

    editirebis gaketeba

--- notes.js
+++ notes.js
@@ -1,3 +1,14 @@
+//დავალება:  შექმენით ნოუთების აპლიკაცია. სადაც მომხარებელს შეეძლება ნოუთების (ჩანაწერების) დამატება, წაშლა, ედიტირება და ნახვა. 
+//დავალება შედგება ორინაწილისგან:
+//ერთ მხარეს უნდა იყოს შექმნილი ნოთების სია
+//მეორე მხარეს თვითონ ნოუთის ტექსტი. 
+//სიის ელემენტის სახელი არის ნოუთის პირველი ხაზი და იქვე უნდა ეწეროს ნოუთის შექმნის ან ბოლო რედაქტირების დრო. 
+//თუ ნოუთში არაფერი წერია მაშინ დაეწეროს "უსათაურო".
+//არსებულ ნოუთზე დაკლიკების შემთხვევაში შემეძლოს მისი ნახვაც და შეცვლაც. დაგვჭირდება ნოუთის შექმნის ღილაკი და წაშლის ღილაკი.
+//HTML: button, textarea, ul
+//JAVASRIPT: Events (click, change), Date, 
+//document-ზე მოქმედებებისთვის (ელემენტის შექმნა, ჩამატება, რედაქტირება, წაშლა) 
+
 const buuttonNewNote=document.getElementById("new-note");
 const buttonDelete=document.getElementById("delete");
 const newText=document.getElementById("text");
@@ -5,44 +16,56 @@ const buttonSave=document.getElementById("save");
 const noteSaveUl=document.getElementById("save-note");
 let date=document.getElementById("date");
 
-
 let notes=[];  
 
+
 buuttonNewNote.addEventListener('click', function(){
-    console.log("create");
-    newText.style.display = "block"; //css მქონდა დისპლეის დამალვა და ამით გამოვაჩინეთ   
+    
+    // console.log("create");
+    newText.style.display = "block"; //css მქონდა დისპლეის დამალვა და ამით გამოვაჩინეთ 
+
+    date.textContent=Date().substring(4, 25);
+    setInterval(() => {
+        date.textContent=Date().substring(4, 25); // გამოჩნდეს გახსნის დრო
+    }, 1000);      
 });
+
 
 buttonSave.addEventListener('click', function(){
     if (newText.value!=""){ 
-        console.log("save");
+        // console.log("save");
         const words = newText.value.substring(0, 13); // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს
         const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li            
-        li.textContent=words+"..."; // სათაურს დაემატოს ...
+        li.textContent=words+"...  "+ Date().substring(15, 25); // სათაურს დაემატოს ...და შენახვის დრო
         noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
-        const id = Date.now();
+        const id = Date.now(); // მილიწამები ავიღოთ id, არასდროს არ გამეორდება
         notes.push({id: id, text: newText.value}); // li ჩაფუშოს notes-ს მასივში
         li.id = id;
+
         li.addEventListener("click", editNote);
-        console.log(notes); 
+        // console.log(edit); 
 
         newText.style.display = "none"; //შენახვის შემდეგ დისპლეი დაიმალოს  
-        newText.value = ""; // დისპლეის ტექსტი განულდეს
-
-        date.textContent = Date().substring(4, 25); // გამოჩნდეს შენახვის დრო 
-    };        
-      
+        newText.value = ""; // დისპლეის ტექსტი განულდეს        
+    };       
 });   
 
+
 function editNote(){
-    
-    
+    newText.style.display = "block";     // ვაჩენთ ახალ დისპლეის
+    let x=this.id;                       //ვინახავთ აქტიური notes აიდს
+    for(let i=0; i<notes.length; i++){
+        if(notes[i].id==x){              //ვეძებთ li-ში ამ აიდს
+            newText.value=notes[i].text; // ახალ დისპლეიში ვსვავთ შენახული li-ს ტექსტს
+            console.log(x);
+            buttonSave.addEventListener('click', function(){
+                notes[i].text=newText.value;
+            });
+        };   
+    };
+
 };
-
-
-// if (li != ''){
-//     li.addEventListener('click', function(){   
-//         console.log("corect"); 
-        
-// });
-// }; 
+    // newText.value=x;
+    // newText.value=notes[0].text;
+    
+// alert("დარწმუნებული ხარ?");
--- test.js
+++ test.js
@@ -3,6 +3,15 @@
 // const words = str.split(' ');
 // console.log(words[0],''+words[1], ''+words[2]);
 
-var anyString = 'Mozilla';
-console.log(anyString.substring(0, 9));
-// console.log(anyString.substring(3, 0));
+// var anyString = 'Mozilla';
+// console.log(anyString.substring(0, 9));
+// // console.log(anyString.substring(3, 0));
+let a=[1, 5, 4, 2];
+let y=0;
+let x=2;
+    for(let i=0; i<a.length; i++){
+        if(x=a[i]){
+            y=a[i];
+        };
+    };
+console.log(y);
