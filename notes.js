9d22bd8f78454a48c6603e6a26594a2ad69aae06
Natia <n.nakashidze09@gmail.com>
2020-10-05 13:40:24 +0400

    mgoni meshvela da sasrulda

--- notes.js
+++ notes.js
@@ -15,87 +15,81 @@ const newText=document.getElementById("text");
 const buttonSave=document.getElementById("save");
 const noteSaveUl=document.getElementById("save-note");
 let date=document.getElementById("date");
-
 let notes=[];  
+let myNote = null; // null შევინახეთ ცვლადში
 
 
-buuttonNewNote.addEventListener('click', function(){
-    
-    console.log("create");
-    newText.style.display = "block"; //css მქონდა დისპლეის დამალვა და ამით გამოვაჩინეთ 
-
-    date.textContent=Date().substring(4, 25);
+date.textContent=Date().substring(4, 25);
     setInterval(() => {
         date.textContent=Date().substring(4, 25); // გამოჩნდეს გახსნის დრო
-    }, 1000);      
+    }, 1000);
+
+buuttonNewNote.addEventListener('click', function(){
+    console.log("create");
+    newText.style.display = "block"; //css მქონდა დისპლეის დამალვა და ამით გამოვაჩინეთ 
 });
 
 
 buttonSave.addEventListener('click', function(){
-    if (newText.value!=""){ 
-        console.log("save");
-        const words = newText.value.substring(0, 13); // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს
-        const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li            
-        li.textContent=words+"...  "+ Date().substring(15, 25); // სათაურს დაემატოს ...და შენახვის დრო
-        noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
-        const id = Date.now(); // მილიწამები ავიღოთ id, არასდროს არ გამეორდება
-        notes.push({id: id, text: newText.value}); // li ჩაფუშოს notes-ს მასივში
-        li.id = id;
-
-        li.addEventListener("click", editNote);
-        console.log('edit');          
-
-        newText.style.display = "none"; //შენახვის შემდეგ დისპლეი დაიმალოს  
-        newText.value = ""; // დისპლეის ტექსტი განულდეს    
-
-    }else{
-        const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li               
-        li.textContent="ცარიელი "+ Date().substring(15, 25); // სათაურს დაემატოს ...და შენახვის დრო
-        noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
-        const id = Date.now(); // მილიწამები ავიღოთ id, არასდროს არ გამეორდება
-        notes.push({id: id, text: newText.value}); // li ჩაფუშოს notes-ს მასივში
-        li.id = id;
-
-        li.addEventListener("click", editNote);
-        console.log('edit');        
-
-        newText.style.display = "none"; //შენახვის შემდეგ დისპლეი დაიმალოს 
+    if(myNote !== null){ //თუ აქტიური Note არ აის null
+        document.getElementById(myNote.id).remove(); //მოშალოს ეს Note html-ში
+        let x=myNote.id;
+        console.log(x);
+        for (let i=0; i<notes.length; i++){ // მოშალოს ეს Note js-ში
+            if(notes[i].id==x){
+                notes[i]=notes[notes.length-1];
+                notes.pop();
+                console.log(notes);
+            }
+        }
     }
+    console.log("save");
+    const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li     
+    const words = newText.value.substring(0, 13); // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს               
+    li.textContent= (words == "" ? "ცარიელი " : words+"...  ") + Date().substring(15, 25); 
+    // თუ ცარიელია, დაწეროს "ცალიელი", თუ არა: მისი ტექსტი და სათაურს დაემატოს ...და შენახვის დრო
+    noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
+    const id = Date.now(); // მილიწამები ავიღოთ id (არასდროს არ გამეორდება)
+    notes.push({id: id, text: newText.value}); // li ჩაფუშოს notes-ს მასივში
+    li.id = id;                 
+
+    newText.style.display = "none"; //შენახვის შემდეგ დისპლეი დაიმალოს  
+    newText.value = ""; // დისპლეის ტექსტი განულდეს   
+    
+    li.addEventListener("click", editNote); // აქედან გაეშვას li (ამ ფუნქციაში არის შექმნილი li)
+    console.log('edit');   
+    myNote = null;  // შენახვის შემდეგ Note ტექსტი გაცარიელდეს
 })   
 
 
 function editNote(){
     newText.style.display = "block";     // ვაჩენთ ახალ დისპლეის
     let x=this.id;                       //ვინახავთ აქტიური notes აიდს
-    for(let i=0; i<notes.length; i++){
-        if(notes[i].id==x){              //ვეძებთ li-ში ამ აიდს
-            newText.value=notes[i].text; // ახალ დისპლეიში ვსვავთ შენახული li-ს ტექსტს 
-            console.log(newText.value);
-            notes[i].text=newText.value; // li-ში ვინახავთ ბოლო დაკორექტირებულ ტექსტს
-        };   
-    };
+    myNote = notes.find(function(n){
+       return n.id == x;
+    });
+    console.log(myNote, notes, x);
+
+    newText.value=myNote.text; // ახალ დისპლეიში ვსვავთ შენახული li-ს ტექსტს 
+    console.log(newText.value);
+    myNote.text=newText.value;    
 };
 
-// li.addEventListener("click", deleteNote); //სად ჩავსვა ??????????
-// console.log('edit');
+buttonDelete.addEventListener('click', function(){
+    alert(':('); 
+    if(myNote !== null){ //თუ აქტიური Note არ აის null
+        document.getElementById(myNote.id).remove(); //მოშალოს ეს Note
+        newText.style.display = "none"; //მოშლის შემდეგ დისპლეი დაიმალოს  
+        newText.value = ""; // დისპლეის ტექსტი განულდეს
 
-
-function deleteNote(){
-    alert('კარგად დაფიქრდი :(');
-
-    newText.style.display = "block";     // ვაჩენთ ახალ დისპლეის
-    let x=this.id;  //ვინახავთ აქტიური notes აიდს
-    console.log(x);
-    for(let i=0; i<notes.length; i++){
-        if(notes[i].id==x){   //ვეძებთ li-ში ამ აიდს
-            newText.value=notes[i].text; // ახალ დისპლეიში ვსვავთ შენახული li-ს ტექსტს
-               
-            notes[i]=notes[notes.length-1]; // მასივის ბოლო ელემენტს ვინახავთ აქტიურ i ელემენტში
-            notes.pop();
-            // removeChild(this.li); // არ იშლება ??????????
-        };    
-    };      
-};
-
-// შენახვა ხდება მხოლოდ ახალ notes-ში (სადაც ედიტირება ხდება იქ არა)
-// მოშვლის ღილაკი არ მუშაობს
+        let x=myNote.id;
+        console.log(x);
+        for (let i=0; i<notes.length; i++){
+            if(notes[i].id==x){
+                notes[i]=notes[notes.length-1];
+                notes.pop();
+                console.log(notes);
+            }
+        }
+    }
+})
