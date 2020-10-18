679496b163d42262224cccc97bd7ae33872fc84f
Natia <n.nakashidze09@gmail.com>
2020-10-18 23:22:36 +0400

    dasrulebuli, gamosatania funqciebi

--- notes.js
+++ notes.js
@@ -26,30 +26,43 @@ const search=document.getElementById("search");
 
 let myNote = null; // null შევინახეთ ცვლადში
 let taimer; // setInterval უნდა შევინახო
-let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
+let notes = JSON.parse(localStorage.getItem("notes")) ?? []; //ცვლადში ვინახავ ბრაუზერში შენახულ მასივს ან ცარიელ მასივს
 
-
-for(let i = 0; i < notes.length; i++) {
-    const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li     
-    const words = notes[i].text.substring(0, 13); // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს               
-    li.textContent= (words == "" ? "ცარიელი " : words+"...  ") + notes[i].date; 
-    // თუ ცარიელია, დაწეროს "ცალიელი", თუ არა: მისი ტექსტი და სათაურს დაემატოს ...და შენახვის დრო
-    noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
-    const id = notes[i].id; // მილიწამები ავიღოთ id (არასდროს არ გამეორდება)
-    li.id = id;
-    li.addEventListener("click", editNote); // აქედან გაეშვას li (ამ ფუნქციაში არის შექმნილი li)
+//ბრაუზერის გაშვებისას li-ში გადმოიტანოს შენახული ნოუთები ან მექნება ცარიელი სიმრავლე:
+function displayNotes(notes){ // გამოიყენება ბრაუზერის გაშვების დროს და მოძებნის დროს
+    for(let i = 0; i < notes.length; i++) {
+        const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li     
+        const words = notes[i].text.split('\n')[0]; // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს  
+        const title = document.createElement("p");     
+        const time = document.createElement("span");        
+        title.textContent= (words == "" ? "ცარიელი " : words);
+        time.textContent = notes[i].dateSatauri;
+        li.appendChild(title);
+        li.appendChild(time);  
+        // თუ ცარიელია, დაწეროს "ცალიელი", თუ არა: მისი ტექსტი და სათაურს დაემატოს ...და შენახვის დრო
+        noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
+        const id = notes[i].id; // მილიწამები ავიღოთ id (არასდროს არ გამეორდება)
+        li.id = id;
+        li.addEventListener("click", editNote); // აქედან გაეშვას li (ამ ფუნქციაში არის შექმნილი li)
+    }
 }
 
+displayNotes(notes);
+
+taimerInterval(); //ბრაუშერის გაშვების დროს თაიმერი ჩაირთოს
 
 function taimerInterval (){
     date.textContent=Date().substring(4, 25);
-    taimer=setInterval(() => {
-    date.textContent=Date().substring(4, 25); // გამოჩნდეს გახსნის დრო
-}, 1000);
+    taimer = setInterval(() => {
+        date.textContent=Date().substring(4, 25); // გამოჩნდეს გახსნის დრო
+    }, 1000);
 }
+
     
 buuttonNewNote.addEventListener('click', function(){
-    taimerInterval();
+    if(!taimer) {
+        taimerInterval(); // ტაიმერი ჩაირთოს მხოლოდ მაშინ, როცა აქტიური ტაიმერი არ არის
+    }    
     console.log("create");
     newText.style.display = "block"; //css მქონდა დისპლეის დამალვა და ამით გამოვაჩინეთ 
     newText.value = ""; // დისპლეის ტექსტი განულდეს  
@@ -71,36 +84,39 @@ buttonSave.addEventListener('click', function(){
         };
     };
     console.log("save");
-    const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li     
-    const words = newText.value.substring(0, 13); // სათაურად ვაჩვენებ პირველ 14 სიმბოლოს               
-    li.textContent= (words == "" ? "ცარიელი " : words+"...  ") + Date().substring(15, 21); 
-    // თუ ცარიელია, დაწეროს "ცალიელი", თუ არა: მისი ტექსტი და სათაურს დაემატოს ...და შენახვის დრო
-    noteSaveUl.appendChild(li); // Ul-ში დაამატოს შვილი li
+    const li = document.createElement("li"); // htm-ში დაემატა ახალი ელემენტი li
+    const title = document.createElement("p"); // შევინახო ტექსტი    
+    const time = document.createElement("span"); // შევინახო დრო     
+    const words = newText.value.split('\n')[0]; // ტექსტა დაშლის აბზაცებად და შეინახავს სიმრავლის სახით, ვღებულობთ 0 ინდექს               
+    title.textContent= (words == "" ? "ცარიელი " : words); // თუ ცარიელია, დაწეროს "ცალიელი", თუ არა: მისი ტექსტი
+    time.textContent = Date().substring(15, 21);
+    li.appendChild(title); // li-ში დაემატოს P ტექსტი
+    li.appendChild(time); // li-ში დაემატოს span დრო
+    noteSaveUl.prepend(li); // Ul-ში დაამატოს შვილი li დასაწყისში
     const id = Date.now(); // მილიწამები ავიღოთ id (არასდროს არ გამეორდება)
     li.id = id;
-    notes.push({id: id, text: newText.value, date: Date().substring(4, 21)}); // li ჩაფუშოს notes-ს მასივში
+    notes.unshift({id: id, text: newText.value, date: Date().substring(4, 21), dateSatauri: Date().substring(15, 21)}); 
+    // li ჩაფუშოს notes-ს მასივში დასაწყისში
     localStorage.setItem("notes", JSON.stringify(notes)); // შევინახე ბრაუზერში
     newText.style.display = "none"; //შენახვის შემდეგ დისპლეი დაიმალოს  
     newText.value = ""; // დისპლეის ტექსტი განულდეს  
-    
-    li.addEventListener("click", editNote); // აქედან გაეშვას li (ამ ფუნქციაში არის შექმნილი li)
-   
-    console.log('edit');   
-    
     myNote = null;  // შენახვის შემდეგ Note ტექსტი გაცარიელდეს
-   
+    taimerInterval(); // ახალი დრო ჩაირთოს
+
+    li.addEventListener("click", editNote); // აქედან გაეშვას li (ამ ფუნქციაში არის შექმნილი li)
+    console.log('edit');      
 }); 
 
 function editNote(){
     newText.style.display = "block";     // ვაჩენთ ახალ დისპლეის
     let x=this.id;                       //ვინახავთ აქტიური notes აიდს
-    myNote = notes.find(function(n){
+    myNote = notes.find(function(n){  //notes-ში მოძებნოს ისეთი ობიექტი, რომლის id არის x
        return n.id == x;
     });
     console.log(myNote, notes, x);
     newText.value=myNote.text; // ახალ დისპლეიში ვსვავთ შენახული li-ს ტექსტს 
-    clearInterval(taimer);
-    date.textContent=myNote.date;    
+    clearInterval(taimer); // თაიმერი გავთიშედ
+    date.textContent=myNote.date; // დავაფიქსირეთ შენახვის დრო  
     console.log(newText.value);    
 };
 
@@ -116,16 +132,20 @@ buttonDelete.addEventListener('click', function(){
                 if(notes[i].id==x){
                     notes[i]=notes[notes.length-1];
                     notes.pop();
+                    localStorage.setItem("notes", JSON.stringify(notes)); // შევინახე ბრაუზერში
                     console.log(notes);
                     myNote = null; // მოშლის შემდეგ Note ტექსტი გაცარიელდეს
                     clearInterval(taimer);
-                    // newText.style.display = "block";
                 };
             };
         };
+        taimerInterval(); // ჩაირთოს ახალი დრო
     };   
 });
 
-search.addEventListener('keyup', () => {
-    console.log(search.value);
+search.addEventListener('keyup', () => { //ღილაკზე ხელის აღების დროს მოხდეს მოსმენა 
+    const filtered = notes.filter(note => note.text.includes(search.value)); //includes: ტექსტში ეძებს ტექსტს
+    //სიმარვლის სახით შემინახოს: notes-ში გამიფილტრე ისეთი note-ები, რომლის ტექსტიც შეიცავს სერჩის ველიუს
+    noteSaveUl.textContent = ""; //Ul გაცარიელდეს (ყველა li მოიშალოს)
+    displayNotes(filtered); // ძებნის დროს li-ში  შემინახოს ახალი მასივი (რომლის ძებნის ტექსტიც ემთხვევა)
 });
--- test.js
+++ test.js
@@ -10,3 +10,21 @@ let x = "gamarjoba vakho"
 undefined
 x.includes("ba");
 true
+
+
+search.addEventListener('keyup', () => { //ღილაკზე ხელის აღების დროს მოხდეს მოსმენა
+    const filtered = notes.filter(note => note.text.includes(search.value)); //notes-ში გამიფილტრე ისეთი note-ები, რომლის ტექსტიც შეიცავს სერჩის ველიუს
+    noteSaveUl.textContent = "";
+    displayNotes(filtered);
+});
+
+
+
+search.addEventListener('keyup', () => { //ღილაკზე ხელის აღების დროს მოხდეს მოსმენა 
+    const filtered = notes.filter (note => note.text.includes (search.Value)); //includes: ტექსტში ეძებს ტექსტს
+    //სიმარვლის სახით შემინახოს: notes-ში გამიფილტრე ისეთი note-ები, რომლის ტექსტიც შეიცავს სერჩის ველიუს
+    noteSaveUl.textContent = ""; //Ul გაცარიელდეს (ყველა li მოიშალოს)
+    displayNotes(filtered); // ძებნის დროს li-ში  შემინახოს ახალი მასივი (რომლის ძებნის ტექსტიც ემთხვევა)
+});
+
+// firstLine = theString.split('\n')[0];
